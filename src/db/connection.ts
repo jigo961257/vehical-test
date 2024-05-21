b97a22d61483ed84962d3839a08b61415fe71045
JGY <jigar.goyani@manektech.com>
2024-05-21 19:58:35 +0530

    add-initial-connect

---
+++ src/db/connection.ts
@@ -0,0 +1,27 @@
+import { Connection, Request, ConnectionConfiguration } from 'tedious'
+
+// try to updaet the post if it's nessaory to change it.
+const config: ConnectionConfiguration = {
+    server: 'localhost',
+    authentication: {
+        type: 'default',
+        options: {
+            userName: 'your_username', // update me
+            password: 'your_password' // update me
+        }
+    },
+    options: {
+        database: "CVISDB1",
+    }
+}
+
+const connection = new Connection(config)
+
+connection.on('connect', (err) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log("connect the DB.");
+
+    }
+})
